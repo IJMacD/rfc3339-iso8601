26c5f29e97f6585d74e1199cd6ac9b53d064bd02
Iain MacDonald <IJMacD@gmail.com>
2024-12-23 21:04:38 +0800

    Add a couple of missing formats
    Add 9 dp for seconds

--- src/formats/iso.js
+++ src/formats/iso.js
@@ -1,119 +1,119 @@
 import { crossJoin } from "../util/crossJoin";
 
 export const date = [
-    "%Y-%M-%D",
-    "%C",
-    "%X",
-    "%Y",
-    "%Y-%M",
-    "%Y-%O",
-    "%V-W%W",
-    "%V-W%W-%w",
-    "%Y%M%D",
-    "%Y%O",
-    "%VW%W",
-    "%VW%W%w",
+  "%Y-%M-%D",
+  "%C",
+  "%X",
+  "%Y",
+  "%Y-%M",
+  "%Y-%O",
+  "%V-W%W",
+  "%V-W%W-%w",
+  "%Y%M%D",
+  "%Y%O",
+  "%VW%W",
+  "%VW%W%w",
 ];
 
 const base_time = [
-    "%h",
-    "%,1h",
-    "%.1h",
-    "%h:%m",
-    "%h:%,1m",
-    "%h:%.1m",
-    "%h:%m:%s",
-    "%h:%m:%.1s",
-    "%h:%m:%.2s",
-    "%h:%m:%,3s",
-    "%h:%m:%.3s",
-    "%h:%m:%s,%u",
-    "%h:%m:%s.%u",
+  "%h",
+  "%,1h",
+  "%.1h",
+  "%h:%m",
+  "%h:%,1m",
+  "%h:%.1m",
+  "%h:%m:%s",
+  "%h:%m:%.1s",
+  "%h:%m:%.2s",
+  "%h:%m:%,3s",
+  "%h:%m:%.3s",
+  "%h:%m:%s,%u",
+  "%h:%m:%s.%u",
+  "%h:%m:%.9s",
+  "%h:%m:%,9s",
 ];
 
 const timezone_time = [
-    ...base_time,
-    ...base_time.map(f => `${f}Z`),
-    ...base_time.map(f => `${f}%Z`),
-    ...base_time.map(f => `${f}%Z:%z`),
+  ...base_time,
+  ...base_time.map((f) => `${f}Z`),
+  ...base_time.map((f) => `${f}%Z`),
+  ...base_time.map((f) => `${f}%Z:%z`),
 ];
 
-
-const prefixed_time = [
-    ...timezone_time,
-    ...timezone_time.map(f => `T${f}`),
-];
+const prefixed_time = [...timezone_time, ...timezone_time.map((f) => `T${f}`)];
 
 const basic_time = [
-    ...prefixed_time,
-    ...prefixed_time.map(f => f.replace(/:/g,"")),
-    // Sample of positive 00:00 timezone
-    "%h:%m:%s+00:00",
-    "%h:%m:%.1s+00:00",
-    "%h:%m:%.3s+00:00",
-    "%h:%m:%s.%u+00:00",
+  ...prefixed_time,
+  ...prefixed_time.map((f) => f.replace(/:/g, "")),
+  // Sample of positive 00:00 timezone
+  "%h:%m:%s+00:00",
+  "%h:%m:%.1s+00:00",
+  "%h:%m:%.2s+00:00",
+  "%h:%m:%.3s+00:00",
+  "%h:%m:%s.%u+00:00",
+  "%h:%m:%.9s+00:00",
 ];
 
 export const time = [...new Set(basic_time)];
 
-const full_date = [
-  "%Y-%M-%D",
-  "%V-W%W-%w",
-  "%Y-%O",
-];
+const full_date = ["%Y-%M-%D", "%V-W%W-%w", "%Y-%O"];
 
 export const dateTime = [
-    ...crossJoin(full_date, timezone_time).map(([d, t]) => `${d}T${t}`),
-    ...crossJoin(full_date, timezone_time).map(([d, t]) => `${d}T${t}`.replace(/[-:]/g, "")),
-    // Sample of positive timezone
-    "%Y-%M-%DT%h:%m:%s+08",
-    "%Y-%M-%DT%h:%m:%s+08:45",
-    // Sample of negative timezone
-    "%Y-%M-%DT%h-12",
-    "%Y-%M-%DT%h-12:00",
-    "%Y-%M-%DT%h:%m-12",
-    "%Y-%M-%DT%h:%m-12:00",
-    // Sample of positive 00:00 timezone
-    "%Y-%M-%DT%h:%m:%s+00:00",
-    "%Y-%M-%DT%h:%m:%.3s+00:00",
+  ...crossJoin(full_date, timezone_time).map(([d, t]) => `${d}T${t}`),
+  ...crossJoin(full_date, timezone_time).map(([d, t]) =>
+    `${d}T${t}`.replace(/[-:]/g, "")
+  ),
+  // Sample of positive timezone
+  "%Y-%M-%DT%h:%m:%s+08",
+  "%Y-%M-%DT%h:%m:%s+08:45",
+  // Sample of negative timezone
+  "%Y-%M-%DT%h-12",
+  "%Y-%M-%DT%h-12:00",
+  "%Y-%M-%DT%h:%m-12",
+  "%Y-%M-%DT%h:%m-12:00",
+  // Sample of positive 00:00 timezone
+  "%Y-%M-%DT%h:%m:%s+00:00",
+  "%Y-%M-%DT%h:%m:%.3s+00:00",
 ];
 
 export const period = [
-    "P1Y",
-    "P1,5Y",
-    "P1.5Y",
-    "P1M",
-    "P1W",
-    "P1D",
-    "PT1H",
-    "P1H",
-    "PT1M",
-    "PT1S",
-    "P1S",
-    "PT1,5S",
-    "PT1.5S",
-    "P1Y1M",
-    "P1Y1D",
-    "P1Y1M1D",
-    "P1Y1M1DT1H1M1S",
-    "P1DT1H",
-    "P1MT1M",
-    "P1DT1M",
-    "P1.5W",
-    "P1,5W",
-    "P1DT1.000S",
-    "P1DT1.00000S",
-    "P1DT1H1M1.1S",
-    "P1H1M1.1S",
+  "P1Y",
+  "P1,5Y",
+  "P1.5Y",
+  "P1M",
+  "P1W",
+  "P1D",
+  "PT1H",
+  "P1H",
+  "PT1M",
+  "PT1S",
+  "P1S",
+  "PT1,5S",
+  "PT1.5S",
+  "P1Y1M",
+  "P1Y1D",
+  "P1Y1M1D",
+  "P1Y1M1DT1H1M1S",
+  "P1DT1H",
+  "P1MT1M",
+  "P1DT1M",
+  "P1.5W",
+  "P1,5W",
+  "P1DT1.000S",
+  "P1DT1.00000S",
+  "P1DT1H1M1.1S",
+  "P1H1M1.1S",
 ];
 
-const example_periods = [
-    "P1Y",
-    "P1M",
-    "P1D",
-];
+const example_periods = ["P1Y", "P1M", "P1D"];
 
-const example_dateTimes = crossJoin(full_date, ["%h","%h:%m","%h:%m:%s","%h:%m:%.3s","%h:%mZ"]).map(([d,t]) => `${d}T${t}`);
+const example_dateTimes = crossJoin(full_date, [
+  "%h",
+  "%h:%m",
+  "%h:%m:%s",
+  "%h:%m:%.3s",
+  "%h:%mZ",
+]).map(([d, t]) => `${d}T${t}`);
 
 export const range = [
   ...crossJoin(full_date, example_periods).map(([d, p]) => `${d}/${p}`),
